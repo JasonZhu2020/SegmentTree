4d59b506af584af578d6e3c6bc9168fde3743917
Chenguang <924443976@qq.com>
2022-03-23 11:08:12 +0800

    Initial commit

---
+++ src/NumArray.java
@@ -0,0 +1,20 @@
+public class NumArray {
+
+    private int[] sum; // sum[i]存储前i个元素的和， sum[0] = 0;
+                        // sum[i] 存储nums[0...i-1]的和;
+
+    public NumArray(int[] nums){
+
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum[i + 1] = sum[i] + nums[i];
+        }
+
+    }
+
+    public int sumRange(int i, int j){
+        return sum[j + 1] - sum[i];
+
+    }
+}
